2137cd5c8b79b4233c9a48744ee9504cc866bc57
C N <nu404040@gmail.com>
2022-10-28 06:24:58 -0700

    installing latest neovim

--- setup_all.sh
+++ setup_all.sh
@@ -4,6 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo ./setup_sudo.sh $USER
 sudo ./install_basics.sh
 sudo ./build_ctags.sh
+sudo ./build_latest_neovim.sh
 sudo ./install_vscode.sh
 ./setup_home.sh
 
